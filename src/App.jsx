18d307e9be9fe7c79e4322450b2ac14a6a137770
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-14 15:03:10 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddLog from './components/AddLog'
+import ViewLog from './components/ViewLog'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
- 
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' element={<AddLog />} />
+          <Route path='/view' element={<ViewLog />} />
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
--- src/components/AddLog.jsx
+++ src/components/AddLog.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import NavigationBar from './NavigationBar'
 
 const AddLog = () => {
 
@@ -50,7 +51,7 @@ const AddLog = () => {
 
     return (
         <div>
-
+<NavigationBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
