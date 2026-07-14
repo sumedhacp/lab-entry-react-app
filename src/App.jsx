b6f6a8313d50f0d3861a31f8543e6154d2ea320e
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-14 14:50:23 +0530

    View Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import AddLog from './components/AddLog'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+ 
     </>
   )
 }
